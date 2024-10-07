b7360aba770a64f132508831f1a0cf6db8daa10d
Gora Indiraja Shafa <gora.indiraja@gmail.com>
2024-10-07 11:01:49 +0700

    feat: Add Download Section

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import DefaultLayout from '../../components/Layouts/DefaultLayout'
 import Jumbotron from '../../components/modules/Jumbotron'
 import SectionEnjoy from '../../components/modules/SectionContents/SectionEnjoy'
+import SectionDownload from '../../components/modules/SectionContents/SectionDownload'
 
 const Landing = () => {
   return (
     <DefaultLayout>
         <Jumbotron/>
         <SectionEnjoy/>
+        <SectionDownload/>
     </DefaultLayout>
   )
 }
