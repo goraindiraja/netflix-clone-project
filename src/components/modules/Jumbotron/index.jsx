92438c8c8aa7e6b788d22fe41576dc3fe6e66c91
Gora Indiraja Shafa <gora.indiraja@gmail.com>
2024-10-07 15:24:30 +0700

    fix: Fixing useAtom error at Jumbotron code

--- src/components/modules/Jumbotron/index.jsx
+++ src/components/modules/Jumbotron/index.jsx
@@ -2,6 +2,7 @@ import EachUtils from '@/utils/EachUtils'
 import React from 'react'
 import InputMembership from '@mods/InputMembership'
 
+import { useAtom } from 'jotai'
 import { languageAtom } from '@/jotai/atoms'
 import { JUMBOTRON_IMAGE } from '@/constants/listAsset'
 import { LIST_JUMBOTRON_EN, LIST_JUMBOTRON_ID } from '@/constants/listJumbotron'
