4c80dd7634989ab39456ecad275ef87096035bff
Gora Indiraja Shafa <gora.indiraja@gmail.com>
2024-10-08 16:05:20 +0700

    feat: BrowsePage - Fetch Recommentadion Videos Data at Modal

--- src/components/modules/BrowsePage/Modal/Recommendation.jsx
+++ src/components/modules/BrowsePage/Modal/Recommendation.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import EachUtils from '@/utils/EachUtils'
 
+import { useAtom } from 'jotai'
 import { GoPlay } from 'react-icons/go'
-import { LIST_VIDEO_RECOMMENDATION } from '@/constants/dummyVideo'
+import { idMovieAtom } from '@/jotai/atoms'
+import { getMoviesRecommendation } from '@/utils/getMoviesRecommendation'
 
 const Recommendation = () => {
+    const [idMovie, setIdMovie] = useAtom(idMovieAtom)
+    const [moviesRecommendation, setMoviesRecommendation] = useState([])
+
+    useEffect(()=> {
+        if(idMovie){
+            getMoviesRecommendation({movie_id: idMovie}).then((result) => setMoviesRecommendation(result))
+        }
+
+    }, [idMovie])
     return (
         <div className='px-2 py-4 mt-4'>
             <h2 className='text-2xl text-white font-bold'>Movies Recommendation</h2>
 
             <div className='grid grid-cols-3 gap-2 mt-4'>
                 <EachUtils
-                    of={LIST_VIDEO_RECOMMENDATION}
+                    of={moviesRecommendation}
                     render={(item, index)=>(
                         <div key={index} className='w-full h-auto cursor-pointer rounded-md bg-[#141414]'>
                             <div className='relative'>
                                 <img 
-                                    src={item.image} 
+                                    src={import.meta.env.VITE_BASE_URL_TMDB_IMAGE + item.poster_path} 
                                     alt="" 
-                                    className='w-full h-32 rounded-t-md'
+                                    className='w-full h-48 rounded-t-md'
                                 />
 
                                 <button className='absolute top-10 left-1/2 -translate-x-1/2'>
@@ -29,10 +40,10 @@ const Recommendation = () => {
                             <div className='p-2'>
                                 <div className='flex gap-2'>
                                     <p>{item.release_date}</p>
-                                    <p>{item.runtime}</p>
+                                    <p className='text-green-400/90'>{item.vote_average}</p>
                                 </div>
 
-                                <p className='text-wrap pt-2'>{item.overview}</p>
+                                <p className='text-wrap pt-2 max-h-32 overflow-y-scroll'>{item.overview}</p>
                             </div>
                         </div>
                     )}
